3565d77b3083ae478c1e87c50bbf46af2299b263
saint <damir.dzhubaliev@gmail.com>
2025-07-03 03:08:25 +0600

    Update URLs, settings, and product links
    Changed root URL to use 'home' view and moved 'landing_index' to '/products'. Updated product detail URL pattern to 'products/<slug:slug>/'. Switched database to PostgreSQL, added production ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS, configured static and media file settings, and introduced detailed logging configuration. Updated landing page to add a direct link to the product detail page.

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import landing_index, index, home, about, delivery, feedback, knt_1, product_detail
 
 urlpatterns = [
-    path('', landing_index, name='landing_index'),
+    path('', home, name='home'),
+    path('products', landing_index, name='landing_index'),
     path('index', index, name='index'),
-    path('home', home, name='home'),
     path('about', about, name='about'),
     path('delivery', delivery, name='delivery'),
     path('feedback', feedback, name='feedback'),
-    path('knt_1', knt_1, name='knt_1'),
-    path('product/<slug:slug>/', product_detail, name='product_detail')
+    # path('knt_1', knt_1, name='knt_1'),
+    path('products/<slug:slug>/', product_detail, name='product_detail')
 ]
