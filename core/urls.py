141216230057e42a650924f872658a2d94eae8f4
saint <damir.dzhubaliev@gmail.com>
2025-08-04 21:01:13 +0600

    Update UI text styles and navigation links
    Removed bold font-weight from several .text-danger spans for a cleaner look. Updated contact phone number and replaced product links in the footer with a PDF catalog. Adjusted navigation and menu links to point to relevant pages, removed unused links, and added anchor for payment section. Commented out unused URL in core/urls.py.

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from .views import landing_index, index, home, about, delivery, feedback, knt_1,
 urlpatterns = [
     path('', home, name='home'),
     path('products', landing_index, name='landing_index'),
-    path('index', index, name='index'),
+    # path('index', index, name='index'),
     path('about', about, name='about'),
     path('delivery', delivery, name='delivery'),
     path('feedback', feedback, name='feedback'),
